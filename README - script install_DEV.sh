b8d53f9c95b775f71be443747f1859c07bc1c02f
crj1035 <129164996+crj1035@users.noreply.github.com>
2023-05-22 12:14:41 +0200

    Update README - script install_DEV.sh

--- README - script install_DEV.sh
+++ README - script install_DEV.sh
@@ -1,1 +1,11 @@
 Le script install_DEV.sh sert à réinstaller les outils sur une machine virtuelle pour un développeur 
+
+# sources.list
+# Update
+# VSCode
+# Git
+# Ansible
+# Créé un utilisateur christophe
+# ajoute l'utilisateur au groupe sudo
+# Créé les clés SSH pour christophe afin de se connecter à ansible
+
